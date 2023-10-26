ff5630a1aa697a22d9e63ed65ca44f5ef231adb4
surayachezul <nursuraya99@gmail.com>
2023-10-26 15:07:57 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aspnetcoremvc_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
